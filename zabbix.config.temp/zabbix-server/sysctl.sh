5e28b1f5782d1852aa31a5a9afe41de99300d5e1
ss75710541 <75710541@qq.com>
2016-03-18 11:01:38 +0800

    更新sysctl.sh

--- zabbix.config.temp/zabbix-server/sysctl.sh
+++ zabbix.config.temp/zabbix-server/sysctl.sh
@@ -1,2 +1,5 @@
-sysctl -w kernel.shmmax=4294964295
-sysctl -w kernel.shmall=268435456
+shmmax=`sysctl -a |grep kernel.shmmax|awk '{print $3}'`
+if [ $shmmax -lt 1073741824 ];then
+	sysctl -w kernel.shmmax=4294964295
+	sysctl -w kernel.shmall=268435456
+fi
