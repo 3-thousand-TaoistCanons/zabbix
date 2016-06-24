78d32e243036b02d7b73319986289c4091d9940e
jyliu <75710541@qq.com>
2016-06-24 10:00:09 +0800

    Update get_token.sh

--- app_deploy/get_token.sh
+++ app_deploy/get_token.sh
@@ -2,14 +2,14 @@
 # shurenyun get token
 . ../config.cfg
 
-token=`cat /tmp/sry-api-token 2>/dev/null`
+token=`cat /tmp/sry-api-${SRY_USER}-token 2>/dev/null`
 code=`curl -X GET --header "Accept: application/json" --header "Authorization: $token"  "$BASE_URL/api/v3/auth" 2>/dev/null |python -m json.tool| awk -F ':|"|,|' '/code/{print $4}'`
 if [ -z "$code" ] || [ "$code" -ne 0 ];then
 	token=`curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d "{
-		\"email\":\"${SRY_USER}\",
+		\"name\":\"${SRY_USER}\",
 		\"password\":\"${SRY_PASS}\"
 	}" "$BASE_URL/api/v3/auth" 2>/dev/null |python -m json.tool|awk -F ':|"' '/token/{print $5}' `
-	echo "$token" > /tmp/sry-api-token
+	echo "$token" > /tmp/sry-api-${SRY_USER}-token
 fi
 echo $token
 
